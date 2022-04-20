93d85661dc87197cf896f973d98d283d32905ade
Etienne Tournier <tournierri@eisti.eu>
2022-04-20 21:13:37 +0200

    vote page

--- politica/src/app/components/vote/vote.component.ts
+++ politica/src/app/components/vote/vote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IVote } from 'src/app/interfaces/vote';
 
 @Component({
   selector: 'app-vote',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
+  voteList: Array<IVote> = [];
+  profil: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.profil = {
+      username: "username",
+      lastName: "lastname",
+      firstName: "fisrtname",
+      genre: "male",
+      email: "email",
+      password: "pasword",
+      politicalParty: "party",
+      age: 25,
+      profilPicture: "../../../assets/PDP.png",
+      journalist: true,
+      image: "we don't care",
+      indicator: 3
+    };
+    this.voteList.push({
+      _id: '1',
+      subject: 'Le vote à 16 ans',
+      for_vote: 16,
+      against_vote: 154,
+      author: '5',
+      dateTime: new Date(),
+      closeDate: new Date(),
+    });
+    this.voteList.push({
+      _id: '2',
+      subject: 'la retraite a 65 ans',
+      for_vote: 168,
+      against_vote: 127,
+      author: '15',
+      dateTime: new Date(),
+      closeDate: new Date(),
+    });
   }
-
 }
--- politica/src/app/interfaces/vote.ts
+++ politica/src/app/interfaces/vote.ts
@@ -1,4 +1,5 @@
 export interface IVote {
+    _id: String,
     subject: String,
     for_vote: Number,
     against_vote: Number,
