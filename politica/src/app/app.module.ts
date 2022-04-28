696bf8cbdaa85c1259dbcde3e0a47e8a723f695b
Etienne Tournier <tournierri@eisti.eu>
2022-04-28 21:52:21 +0200

    home page et comment style

--- politica/src/app/app.module.ts
+++ politica/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //import Components
 import { AppComponent } from './app.component';
@@ -22,6 +23,15 @@ import { AutoCompleteModule } from 'primeng/autocomplete';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { InputSwitchModule } from 'primeng/inputswitch';
+import { TooltipModule } from 'primeng/tooltip';
+import { DialogModule } from 'primeng/dialog';
+import { PasswordModule } from 'primeng/password';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CaptchaModule } from 'primeng/captcha';
+import { ToastModule } from 'primeng/toast';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 
 @NgModule({
   declarations: [
@@ -46,6 +56,16 @@ import { InputSwitchModule } from 'primeng/inputswitch';
     InputTextModule,
     ButtonModule,
     InputSwitchModule,
+    TooltipModule,
+    DialogModule,
+    BrowserAnimationsModule,
+    PasswordModule,
+    DropdownModule,
+    InputNumberModule,
+    CaptchaModule,
+    ToastModule,
+    MessageModule,
+    MessagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- politica/src/app/components/home/home.component.ts
+++ politica/src/app/components/home/home.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [MessageService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  displayRegister: Boolean = false;
+  displayLogin: Boolean = false;
+
+  pseudo: String = '';
+  firstname : String = '';
+  lastname: String = '';
+  email : String = '';
+  password: String = '';
+  genre : String = '';
+  age!: number;
+  politicalParti : String = '';
+
+  genreOptions: Array<String>;
+  partiOptions: Array<String>;
+
+  constructor(private messageService: MessageService) { 
+    this.genreOptions = [ 'Homme', 'Femme', 'Je m\'abstiens'];
+    this.partiOptions = [ 'Je m\'abstiens', 'Indécis', 'Reconquête', 'RN', 'LR', 'LREM', 'MoDem', 'PS', 'EELV', 'LFI', 'PCF'];
+  }
 
   ngOnInit(): void {
   }
 
+  showRegisterDialog() {
+    this.displayRegister = true;
+  }
+
+  showLoginDialog() {
+    this.displayLogin = true;
+  }
+
+  showResponse(event: any) {
+    this.messageService.add({severity:'info', summary:'Succees', detail: 'User Responded', sticky: true});
+  }
+
+  login() {
+    this.displayLogin = false;
+    window.location.replace('/debate');
+  }
+
+  register() {
+    this.displayRegister = false;
+    window.location.replace('/profil');
+  }
 }
